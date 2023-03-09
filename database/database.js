2c9bdeb07ada8143627ed3bc6e038cbe172b6ca8
EliasQa <elias.araujo@datasales.io>
2023-03-09 17:40:28 -0300

    mudou o banco

--- database/database.js
+++ database/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 
 
-const connection = new Sequelize("qaprodev", "root", "R3mixCl@nMorph3l", {
-  host: "localhost",
+const connection = new Sequelize("qaprodev", "root", "GHBqdq75748", {
+  host: "node131216-env-0045258.jelastic.saveincloud.net",
   dialect: "mysql",
 });
 /*const connection = new Sequelize("heroku_e85ee26b2cdf713", "b0c96a488c83b4", "3de68a50", {
