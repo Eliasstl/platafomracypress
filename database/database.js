6619f132f5e49e1005d00edd19c54fff8186d9b7
EliasQa <elias.araujo@datasales.io>
2023-03-09 20:23:09 -0300

    melhorando agendar teste

--- database/database.js
+++ database/database.js
@@ -1,12 +1,12 @@
 const Sequelize = require("sequelize");
 
 
-const connection = new Sequelize("qaprodev", "root", "GHBqdq75748", {
-  host: "node131216-env-0045258.jelastic.saveincloud.net",
+const connection = new Sequelize("qaprodev", "root", "R3mixCl@nMorph3l", {
+  host: "localhost",
   dialect: "mysql",
 });
-/*const connection = new Sequelize("heroku_e85ee26b2cdf713", "b0c96a488c83b4", "3de68a50", {
-  host: "us-cdbr-east-06.cleardb.net",
+/*const connection = new Sequelize("qaprodev", "root", "GHBqdq75748", {
+  host: "node131216-env-0045258.jelastic.saveincloud.net",
   dialect: "mysql",
 });
 */
--- routers/agendamento/agendamentoControle.js
+++ routers/agendamento/agendamentoControle.js
@@ -12,26 +12,11 @@ const adminAuto = require("../../middleware/autorizar");
 const moment = require('moment');
 
 
-router.post('/rodartestesagendados', (req, res) => {
-  const { data, hora, corpo } = req.body;
-
-  const mensagemProgramada = moment(`${data} ${hora}`, 'DD/MM/YYYY HH:mm');
-
-  const diff = mensagemProgramada.diff(moment());
-
-  setTimeout(() => {
-    const diaDaSemana = mensagemProgramada.format('dddd');
-    console.log(`Mensagem enviada no ${diaDaSemana}: ${corpo}`);
-  }, diff);
-
-  res.send('Mensagem agendada com sucesso!');
-});
-
 
 
 
 router.post(
-  "/salvaragendamento/:idempresa/:idagendamento",
+  "/salvaragendamento/:idempresa/",
   adminAuto,
   (req, res) => {
     var idagendamento = req.params.idagendamento;
@@ -53,139 +38,19 @@ router.post(
     var data = req.body.data;
     var nomeagendamento = req.body.nomeagendamento;
     var tempo = req.body.tempo;
+    res.render("agenda", { idempresa });
 
-    let diasDaSemana = [
-      "domingo",
-      "segunda",
-      "terça",
-      "quarta",
-      "quinta",
-      "sexta",
-      "sabado",
-    ];
-
-    let horarios = [];
-
-    diasDaSemana.forEach((dia) => {
-      let horario = {};
-      horario.dia = dia;
-
-      switch (dia) {
-        case "domingo":
-          horario.horainicio = domingohorainicio;
-          horario.horafim = domingohorafim;
-          break;
-        case "segunda":
-          horario.horainicio = segundahorainicio;
-          horario.horafim = segundahorafim;
-          break;
-        case "terça":
-          horario.horainicio = tercahorainicio;
-          horario.horafim = tercahorafim;
-          break;
-        case "quarta":
-          horario.horainicio = quartahorainicio;
-          horario.horafim = quartahorafim;
-          break;
-        case "quinta":
-          horario.horainicio = quintahorainicio;
-          horario.horafim = quintahorafim;
-          break;
-        case "sexta":
-          horario.horainicio = sextahorainicio;
-          horario.horafim = sextahorafim;
-          break;
-        case "sabado":
-          horario.horainicio = sabadohorainicio;
-          horario.horafim = sabadohorafim;
-          break;
-      }
-
-      horarios.push(horario);
-    });
-
-    Agendamento.findAll({ where: { idagendamento: idagendamento } }).then(
-      (agendamentos) => {
-        agendamentos.forEach(async (agendar) => {
-          if (agendar) {
-            horarios.forEach(async (horario) => {
-              if (horario.horainicio && horario.horafim) {
-                // atualiza a tabela Agendamento com as informações do horário deste dia
-                await Agendamento.create({
-                  idempresa: idempresa,
-                  nometeste: agendar.nometeste,
-                  nomeagendamento: nomeagendamento,
-                  diasemana: horario.dia.toLowerCase(),
-                  datafim: data,
-                  horainicio: horario.horainicio,
-                  horafim: horario.horafim,
-                  tempo: tempo,
-                  executar: "nao",
-                });
-              }
-            });
-            agendar.destroy();
-          }
-        });
-      }
-    );
-
-    res.render("acessar");
   }
 );
 
-router.post("/agenda/:idempresa", adminAuto, async (req, res) => {
-  const idempresa = req.params.idempresa;
-  const selecionados = req.body["selecionado[]"];
-  const dataatual = "id" + Date.now().toString(); // obtém a data atual
-
-  try {
-    if (Array.isArray(selecionados)) {
-      for (const selecionado of selecionados) {
-        await Agendamento.create({
-          idempresa: idempresa,
-          nometeste: selecionado,
-          nomeagendamento: "Nome do agendamento",
-          diasemana: "Dia da semana",
-
-          horainicio: "Hora de início",
-          horafim: "Hora de fim",
-          tempo: "Tempo",
-          executar: "nao",
-          idagendamento: dataatual,
-        });
-      }
-    } else {
-      await Agendamento.create({
-        idempresa: idempresa,
-        nometeste: selecionados,
-        nomeagendamento: "Nome do agendamento",
-        diasemana: "Dia da semana",
-
-        horainicio: "Hora de início",
-        horafim: "Hora de fim",
-        tempo: "Tempo",
-        executar: "nao",
-        idagendamento: dataatual,
-      });
-    }
-
-    res.render("agenda", { idempresa, dataatual });
-  } catch (error) {
-    console.log("Erro ao inserir dados na tabela Agendamento: " + error);
-    res.status(500).send("Erro ao inserir dados na tabela Agendamento");
-  }
+router.get("/agenda/:idempresa", (req, res) => {
+  const idempresa = req.params.idempresa
+  
+    res.render("agenda", { idempresa });
+ 
 });
 
-router.get("/selecionartesteagenda/:idempresa", adminAuto, (req, res) => {
-  const idempresa = req.params.idempresa;
-  Nometeste.findAll().then((testes) => {
-    res.render("selecionartesteagenda", {
-      idempresa,
-      testes,
-    });
-  });
-});
+
 
 // Define a rota para /agendamento
 router.post("/agendamento", (req, res) => {
