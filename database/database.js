9b5e37173390e99d0f3c2d4fe21798a2dade1cda
EliasQa <elias.araujo@datasales.io>
2023-03-10 16:27:11 -0300

    webhook

--- database/database.js
+++ database/database.js
@@ -9,7 +9,11 @@ const connection = new Sequelize("qaprodev", "root", "GHBqdq75748", {
   host: "10.100.47.71",
   dialect: "mysql",
 });
-
+/*const connection = new Sequelize("qaprodev", "root", "GHBqdq75748", {
+  host: "node131216-env-0045258.jelastic.saveincloud.net",
+  dialect: "mysql",
+});
+*/
 
 
 module.exports = connection;
